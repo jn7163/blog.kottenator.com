959079adde07c991dfd9537e6d03ed3dce6dbf62
Rostyslav Bryzgunov <kottenator@gmail.com>
2016-11-09 20:36:55 -0500

    PEP8 fixes

--- src/project/blog/migrations/0001_initial.py
+++ src/project/blog/migrations/0001_initial.py
@@ -1,43 +1,49 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models, migrations
 
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True, serialize=False, primary_key=True, verbose_name='ID'
+                    )
+                ),
                 ('title', models.CharField(max_length=2000)),
                 ('slug', models.SlugField(unique=True, max_length=2000)),
                 ('summary', models.TextField(blank=True)),
                 ('body', models.TextField(blank=True)),
-                ('published_at', models.DateTimeField(null=True, blank=True)),
+                ('published_at', models.DateTimeField(null=True, blank=True))
             ],
             options={
-                'db_table': 'blog_article',
-            },
+                'db_table': 'blog_article'
+            }
         ),
         migrations.CreateModel(
             name='Label',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True, serialize=False, primary_key=True, verbose_name='ID'
+                    )
+                ),
                 ('title', models.CharField(max_length=2000)),
-                ('slug', models.SlugField(unique=True, max_length=2000)),
+                ('slug', models.SlugField(unique=True, max_length=2000))
             ],
             options={
-                'db_table': 'blog_label',
+                'db_table': 'blog_label'
             },
         ),
         migrations.AddField(
             model_name='article',
             name='labels',
-            field=models.ManyToManyField(to='blog.Label'),
-        ),
+            field=models.ManyToManyField(to='blog.Label')
+        )
     ]
